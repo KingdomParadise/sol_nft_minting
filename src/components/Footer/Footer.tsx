f71b0c982753e3718f9247d8f1e8ff45ab0de80a
KingdomParadise <ericlee.apollo@gmail.com>
2021-11-15 18:11:10 -0800

    finished minting

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return(
+    <footer className="py-8">
+      <p className="text-2xl uppercase text-black font-bold">nftatts by walking designs</p>
+      <p className="text-md text-gray-600 mt-4 mb-5">walkingdesigns@gmail.com</p>
+      <p className="bg-gray-200 text-black py-2">©2023 by Walking Design</p>
+    </footer>
+  );
+}
+export default Footer;
